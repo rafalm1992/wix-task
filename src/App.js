40f3cf8c4c88f315fe6a23e1a6c5d2fd49620713
Rafal Mlynski <rafal.mlynski.2016@gmail.com>
2019-12-18 16:54:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let initState = {
+    treeData: [
+      {
+        name: "Lorem",
+        children: [
+          {
+            name: "Ipsum",
+            children: []
+          },
+          {
+            name: "Dolor",
+            children: [
+              {
+                name: "Orci",
+                children: [
+                  {
+                    name: "Quis",
+                    children: [
+                      {
+                        name: "Odio",
+                        children: []
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            name: "Sit",
+            children: [
+              {
+                name: "Amet",
+                children: []
+              },
+              {
+                name: "Consectetur",
+                children: []
+              }
+            ]
+          },
+          {
+            name: "Adipiscing",
+            children: [
+              {
+                name: "Elit",
+                children: [
+                  {
+                    name: "Vestibulum",
+                    children: []
+                  },
+                  {
+                    name: "Vitae",
+                    children: []
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+
+  const [data, setData] = useState(initState);
+  // This function handles arrays and objects
+  // function eachRecursive(obj) {
+  //   for (var k in obj) {
+  //     if (typeof obj[k] == "object" && obj[k] !== null) eachRecursive(obj[k]);
+  //     else {
+  //       return console.log(k);
+  //     }
+  //   }
+  // }
+  // eachRecursive(initState.treeData[0]);
+
+  function listHtml(children) {
+    return (
+      <ul>
+        {children.map(node => {
+          if (node.children.length > 0) {
+            listHtml(node.children);
+          }
+          return <li key={node}>k</li>;
+        })}
+      </ul>
+    );
+  }
+
+  return <div className="App">{listHtml(data.treeData)}</div>;
 }
 
 export default App;
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,76 @@
+let initState = {
+  treeData: [
+    {
+      id: 1,
+      name: "Lorem",
+      children: [
+        {
+          id: 2,
+          name: "Ipsum",
+          children: []
+        },
+        {
+          id: 3,
+          name: "Dolor",
+          children: [
+            {
+              id: 31,
+              name: "Orci",
+              children: [
+                {
+                  id: 311,
+                  name: "Quis",
+                  children: [
+                    {
+                      id: 3111,
+                      name: "Odio",
+                      children: []
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 4,
+          name: "Sit",
+          children: [
+            {
+              id: 41,
+              name: "Amet",
+              children: []
+            },
+            {
+              id: 42,
+              name: "Consectetur",
+              children: []
+            }
+          ]
+        },
+        {
+          id: 5,
+          name: "Adipiscing",
+          children: [
+            {
+              id: 51,
+              name: "Elit",
+              children: [
+                {
+                  id: 511,
+                  name: "Vestibulum",
+                  children: []
+                },
+                {
+                  id: 512,
+                  name: "Vitae",
+                  children: []
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
